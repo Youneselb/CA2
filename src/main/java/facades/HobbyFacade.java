1518e12f310769d7a6f7aa74441484548724eebf
youneselb <youneselb@gmail.com>
2020-10-19 15:48:45 +0200

    hobbyfacade

--- src/main/java/facades/HobbyFacade.java
+++ src/main/java/facades/HobbyFacade.java
@@ -1,6 +1,6 @@
 package facades;
 
-import entities.RenameMe;
+import entities.Hobby;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -10,13 +10,13 @@ import javax.persistence.Persistence;
  *
  * Rename Class to a relevant name Add add relevant facade methods
  */
-public class FacadeExample {
+public class HobbyFacade {
 
-    private static FacadeExample instance;
+    private static HobbyFacade instance;
     private static EntityManagerFactory emf;
     
     //Private Constructor to ensure Singleton
-    private FacadeExample() {}
+    private HobbyFacade() {}
     
     
     /**
@@ -24,10 +24,10 @@ public class FacadeExample {
      * @param _emf
      * @return an instance of this facade class.
      */
-    public static FacadeExample getFacadeExample(EntityManagerFactory _emf) {
+    public static HobbyFacade getHobbyFacade(EntityManagerFactory _emf) {
         if (instance == null) {
             emf = _emf;
-            instance = new FacadeExample();
+            instance = new HobbyFacade();
         }
         return instance;
     }
