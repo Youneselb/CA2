4aacaa598eba6e926917f0b3f24940b711ad60a1
Henrik <Henne3660@hotmail.com>
2020-10-19 16:10:58 +0200

    added address og adressFacade

---
+++ src/main/java/entities/Address.java
@@ -0,0 +1,49 @@
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQuery(name = "Address.deleteAllRows", query = "DELETE from Address")
+public class Address implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String street;
+    private String additionalInfo;
+
+    public Address() {
+    }
+
+    public Address(String street, String additionalInfo) {
+        this.street = street;
+        this.additionalInfo = additionalInfo;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getAdditionalInfo() {
+        return additionalInfo;
+    }
+
+    public void setAdditionalInfo(String additionalInfo) {
+        this.additionalInfo = additionalInfo;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" + "street=" + street + ", additionalInfo=" + additionalInfo + '}';
+    }
+
+}
---
+++ src/main/java/facades/AddressFacade.java
@@ -0,0 +1,34 @@
+package facades;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+public class AddressFacade {
+
+    private static AddressFacade instance;
+    private static EntityManagerFactory emf;
+
+    public static AddressFacade getHobbyFacade(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new AddressFacade();
+        }
+        return instance;
+    }
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    //TODO Remove/Change this before use
+    public long getRenameMeCount() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            long renameMeCount = (long) em.createQuery("SELECT COUNT(r) FROM RenameMe r").getSingleResult();
+            return renameMeCount;
+        } finally {
+            em.close();
+        }
+
+    }
+}
