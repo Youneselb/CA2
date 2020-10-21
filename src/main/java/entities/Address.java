ac65c8f5c332d387dcde1d92f51539cdaaefcbba
youneselb <youneselb@gmail.com>
2020-10-21 10:04:35 +0200

    onetomany

--- src/main/java/entities/Address.java
+++ src/main/java/entities/Address.java
@@ -1,6 +1,8 @@
 package entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 
 @Entity
 @NamedQuery(name = "Address.deleteAllRows", query = "DELETE from Address")
@@ -21,7 +24,10 @@ public class Address implements Serializable {
     private String additionalInfo;
     @ManyToOne(cascade = CascadeType.PERSIST)
     private CityInfo cityinfo;
-
+@OneToMany(mappedBy = "address", cascade = CascadeType.PERSIST)
+    private List<Person> persons = new ArrayList<>();   
+    
+    
     public Address() {
     }
 
