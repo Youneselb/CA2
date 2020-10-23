c8c1f0573a2946e4518aa70e46531837624aef54
Henrik <Henne3660@hotmail.com>
2020-10-23 10:42:04 +0200

    refactor af metode i rest

--- src/main/java/rest/PersonResource.java
+++ src/main/java/rest/PersonResource.java
@@ -61,7 +61,6 @@ public class PersonResource {
     
     @PUT
     @Path("edit/{id}")
-    @Produces({MediaType.APPLICATION_JSON})
     @Consumes({MediaType.APPLICATION_JSON})
     public String editPerson(@PathParam("id") long id, String person) throws MissingInputException, PersonNotFoundException {
         PersonDTO pDTO = GSON.fromJson(person, PersonDTO.class);
