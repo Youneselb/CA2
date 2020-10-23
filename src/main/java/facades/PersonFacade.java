afead1d5a78790fcbb3f5a007abb39e98b038931
Henrik <Henne3660@hotmail.com>
2020-10-23 15:36:13 +0200

    fix forsøg igen på getPersonByCity

--- src/main/java/facades/PersonFacade.java
+++ src/main/java/facades/PersonFacade.java
@@ -85,7 +85,7 @@ public class PersonFacade {
 
     public List<PersonDTO> getPersonsByCity(String city) {
         EntityManager em = emf.createEntityManager();
-        Query query = em.createQuery("SELECT a.persons FROM Address a JOIN CityInfo c WHERE c.addresses = :city");
+        Query query = em.createQuery("SELECT p.address FROM Person p JOIN CityInfo c WHERE c.addresses = :city");
         query.setParameter("city", city);
         List<Person> persons = query.getResultList();
         List<PersonDTO> personsDTOs = new ArrayList();
